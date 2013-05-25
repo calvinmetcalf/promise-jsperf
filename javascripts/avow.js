9ef68afaec38b23e7776a4b0ff4f66ca0b15851a
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-05-25 07:19:12 -0400

    non minified avow

--- javascripts/avow.js
+++ javascripts/avow.js
@@ -1,3 +1,212 @@
-(function(c,f){c(function(){function c(a){function g(b){function l(b){j&&e(h(b))}function a(b){j&&(d||r(m,b),e(q(b)))}function e(b){var a=j;j=u;c=b;n(function(){a.forEach(function(b){b(c)})})}var m,c,d,j=[];m=new p(function(b,a){d||(d=!0,k(m));return g(function(l,e){j?j.push(function(c){c.then(b,a).then(l,e)}):n(function(){c.then(b,a).then(l,e)})})});try{b(l,a)}catch(f){a(f)}return m}function p(b){this.then=b;s(this)}function h(b){return b instanceof p?b:b!==Object(b)?f(b):g(function(a,c){n(function(){try{var e=
-b.then;"function"===typeof e?t(e,b,a,c):a(f(b))}catch(d){c(d)}})})}function f(b){var a=new p(function(c){try{return"function"==typeof c?h(c(b)):a}catch(e){return q(e)}});return a}function q(b){var a=new p(function(c,e){try{return"function"==typeof e?h(e(b)):a}catch(d){return q(d)}});return a}var n,k,r,s;n=a.enqueue||d.enqueue;k=a.handled||d.handled;r=a.unhandled||d.unhandled;s=a.protect||d.protect;g.lift=function(b){return g(function(a){a(b)})};g.reject=function(b){return g(function(a,c){c(b)})};
-return g}function h(){}var a,d,k,t,u;a=Function.prototype.bind;t=a.bind(a.call)(a.call);k=f.setTimeout;d={enqueue:"function"===typeof setImmediate?setImmediate.bind(f):"object"===typeof process&&process.nextTick?process.nextTick:"object"===typeof vertx?vertx.runOnLoop:function(a){k(a,0)},unhandled:h,handled:h,protect:h};a=c(d);a.construct=c;return a})})("function"===typeof define&&define.amd?define:function(c){module.exports=c()},this);
+/* Copyright (c) 2012-2013 Brian Cavalier */
+(function(define, global) {
+define(function() {
+
+	var avow, enqueue, defaultConfig, setTimeout, bind, uncurryThis, call, undef;
+
+	bind = Function.prototype.bind;
+	uncurryThis = bind.bind(bind.call);
+	call = uncurryThis(bind.call);
+
+	// Prefer setImmediate, cascade to node, vertx and finally setTimeout
+	/*global setImmediate,process,vertx*/
+	setTimeout = global.setTimeout;
+	enqueue = typeof setImmediate === 'function' ? setImmediate.bind(global)
+		: typeof process === 'object' && process.nextTick ? process.nextTick
+		: typeof vertx === 'object' ? vertx.runOnLoop // vert.x
+			: function(task) { setTimeout(task, 0); }; // fallback
+
+	// Default configuration
+	defaultConfig = {
+		enqueue:   enqueue,
+		unhandled: noop,
+		handled:   noop,
+		protect:   noop
+	};
+
+	// Create the default module instance
+	// This is what you get when you require('avow')
+	avow = constructAvow(defaultConfig);
+
+	// You can use require('avow').construct(options) to
+	// construct a custom configured version of avow
+	avow.construct = constructAvow;
+
+	return avow;
+
+	// This constructs configured instances of the avow module
+	function constructAvow(config) {
+
+		var enqueue, onHandled, onUnhandled, protect;
+
+		// Grab the config params, use defaults where necessary
+		enqueue     = config.enqueue   || defaultConfig.enqueue;
+		onHandled   = config.handled   || defaultConfig.handled;
+		onUnhandled = config.unhandled || defaultConfig.unhandled;
+		protect     = config.protect   || defaultConfig.protect;
+
+		// Add lift and reject methods.
+		promise.lift    = lift;
+		promise.reject  = reject;
+
+		return promise;
+
+		// Return a trusted promise for x.  Where if x is a
+		// - Promise, return it
+		// - value, return a promise that will eventually fulfill with x
+		// - thenable, assimilate it and return a promise whose fate follows that of x.
+		function lift(x) {
+			return promise(function(resolve) {
+				resolve(x);
+			});
+		}
+
+		// Return a rejected promise
+		function reject(reason) {
+			return promise(function(_, reject) {
+				reject(reason);
+			});
+		}
+
+		// Return a pending promise whose fate is determined by resolver
+		function promise(resolver) {
+			var self, value, handled, handlers = [];
+
+			self = new Promise(then);
+
+			// Call the resolver to seal the promise's fate
+			try {
+				resolver(promiseResolve, promiseReject);
+			} catch(e) {
+				promiseReject(e);
+			}
+
+			// Return the promise
+			return self;
+
+			// Register handlers with this promise
+			function then(onFulfilled, onRejected) {
+				if (!handled) {
+					handled = true;
+					onHandled(self);
+				}
+
+				return promise(function(resolve, reject) {
+					handlers
+						// Call handlers later, after resolution
+						? handlers.push(function(value) {
+							value.then(onFulfilled, onRejected).then(resolve, reject);
+						})
+						// Call handlers soon, but not in the current stack
+						: enqueue(function() {
+							value.then(onFulfilled, onRejected).then(resolve, reject);
+						});
+				});
+			}
+
+			// Resolve with a value, promise, or thenable
+			function promiseResolve(value) {
+				if(!handlers) {
+					return;
+				}
+
+				resolve(coerce(value));
+			}
+
+			// Reject with reason verbatim
+			function promiseReject(reason) {
+				if(!handlers) {
+					return;
+				}
+
+				if(!handled) {
+					onUnhandled(self, reason);
+				}
+
+				resolve(rejected(reason));
+			}
+
+			// For all handlers, run the Promise Resolution Procedure on this promise
+			function resolve(x) {
+				var queue = handlers;
+				handlers = undef;
+				value = x;
+
+				enqueue(function () {
+					queue.forEach(function (handler) {
+						handler(value);
+					});
+				});
+			}
+		}
+
+		// Private
+
+		// Trusted promise constructor
+		function Promise(then) {
+			this.then = then;
+			protect(this);
+		}
+
+		// Coerce x to a promise
+		function coerce(x) {
+			if(x instanceof Promise) {
+				return x;
+			} else if (x !== Object(x)) {
+				return fulfilled(x);
+			}
+
+			return promise(function(resolve, reject) {
+				enqueue(function() {
+					try {
+						// We must check and assimilate in the same tick, but not the
+						// current tick, careful only to access promiseOrValue.then once.
+						var untrustedThen = x.then;
+
+						if(typeof untrustedThen === 'function') {
+							call(untrustedThen, x, resolve, reject);
+						} else {
+							// It's a value, create a fulfilled wrapper
+							resolve(fulfilled(x));
+						}
+					} catch(e) {
+						// Something went wrong, reject
+						reject(e);
+					}
+				});
+			});
+		}
+
+		// create an already-fulfilled promise used to break assimilation recursion
+		function fulfilled(x) {
+			var self = new Promise(function (onFulfilled) {
+				try {
+					return typeof onFulfilled == 'function'
+						? coerce(onFulfilled(x)) : self;
+				} catch (e) {
+					return rejected(e);
+				}
+			});
+
+			return self;
+		}
+
+		// create an already-rejected promise
+		function rejected(x) {
+			var self = new Promise(function (_, onRejected) {
+				try {
+					return typeof onRejected == 'function'
+						? coerce(onRejected(x)) : self;
+				} catch (e) {
+					return rejected(e);
+				}
+			});
+
+			return self;
+		}
+	}
+
+	function noop() {}
+
+});
+})(typeof define === 'function' && define.amd ? define : function(factory) { module.exports = factory(); }, this);
