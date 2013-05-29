5da053b946f4151bcb61a0e28d4334aea2346ae0
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-05-29 08:28:08 -0400

    set imidiate

--- javascripts/promiscuous-browser.js
+++ javascripts/promiscuous-browser.js
@@ -1,2 +1,77 @@
-/** @license MIT - ©2013 Ruben Verborgh l*/
-(function(){function e(){var c,u,f={then:function(e,n){return c(e,n)}};return function(){var i=[];c=function(n,r){var t=e();return i.push({d:t,resolve:n,reject:r}),t.promise},u=function(e,v,s){for(var a=0,p=i.length;p>a;a++){var h=i[a],l=h.d,d=h[e];typeof d!==t?l[e](v):r(d,v,l)}c=n(f,v,s),u=o}}(),{resolve:function(e){u("resolve",e,!0)},reject:function(e){u("reject",e,!1)},promise:f}}function n(n,o,c){return function(u,f){var i,v=c?u:f;return typeof v!==t?n:(setTimeout(r.bind(n,v,o,i=e())),i.promise)}}function r(e,n,r){try{var o=e(n);o&&typeof o.then===t?o.then(r.resolve,r.reject):r.resolve(o)}catch(c){r.reject(c)}}var t="function",o=function(){};window.promiscuous={resolve:function(e){var r={};return r.then=n(r,e,!0),r},reject:function(e){var r={};return r.then=n(r,e,!1),r},deferred:e}})();
+/*! Promiscuous; ©2013 Ruben Verborgh @license MIT https://github.com/RubenVerborgh/promiscuous*/
+var promiscuous;
+(function (exports) {
+  var func = "function",
+      noop = function () {};
+
+  function createDeferred() {
+    var handler,
+        changeState,
+        promise = {
+          then: function (onFulfilled, onRejected) {
+            return handler(onFulfilled, onRejected);
+          }
+        };
+
+    (function () {
+      var pending = [];
+      handler = function (onFulfilled, onRejected) {
+        var d = createDeferred();
+        pending.push({ d: d, resolve: onFulfilled, reject: onRejected });
+        return d.promise;
+      };
+      changeState = function (action, value, success) {
+        for (var i = 0, l = pending.length; i < l; i++) {
+          var p = pending[i], deferred = p.d, callback = p[action];
+          if (typeof callback !== func)
+            deferred[action](value);
+          else
+            execute(callback, value, deferred);
+        }
+        handler = createHandler(promise, value, success);
+        changeState = noop;
+      };
+    })();
+
+    return {
+      resolve: function (value)  { changeState('resolve', value, true); },
+      reject : function (reason) { changeState('reject', reason, false); },
+      promise: promise
+    };
+  }
+
+  function createHandler(promise, value, success) {
+    return function (onFulfilled, onRejected) {
+      var callback = success ? onFulfilled : onRejected, result;
+      if (typeof callback !== func)
+        return promise;
+      setImmediate(execute.bind(promise, callback, value, result = createDeferred()));
+      return result.promise;
+    };
+  }
+
+  function execute(callback, value, deferred) {
+    try {
+      var result = callback(value);
+      if (result && typeof result.then === func)
+        result.then(deferred.resolve, deferred.reject);
+      else
+        deferred.resolve(result);
+    }
+    catch (error) {
+      deferred.reject(error);
+    }
+  }
+
+  exports.resolve= function (value) {
+      var promise = {};
+      promise.then = createHandler(promise, value, true);
+      return promise;
+    };
+    exports.reject= function (reason) {
+      var promise = {};
+      promise.then = createHandler(promise, reason, false);
+      return promise;
+    }
+    exports.deferred=createDeferred;
+})(promiscuous);
